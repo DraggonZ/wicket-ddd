5ccdd61a80b34827ebd79d1e6ec8dc68ac30218a
Alexander Morozov <alexander.v.morozov@gmail.com>
2015-03-01 17:16:48 +0600

    Реализация одиночного выбора выбора.

--- src/main/java/promolo/wicket/account/instractructure/presentation/AccountLayout.java
+++ src/main/java/promolo/wicket/account/instractructure/presentation/AccountLayout.java
@@ -1,5 +1,8 @@
 package promolo.wicket.account.instractructure.presentation;
 
+import org.apache.wicket.ajax.IAjaxIndicatorAware;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxIndicatorAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -11,13 +14,21 @@ import promolo.wicket.core.ui.bootstrap.BootstrapFeedbackPanel;
  *
  * @author Александр
  */
-public class AccountLayout extends WebPage {
+public class AccountLayout extends WebPage implements IAjaxIndicatorAware {
+
+    private final AjaxIndicatorAppender indicatorAppender = new AjaxIndicatorAppender();
 
     public AccountLayout(PageParameters parameters) {
         super(parameters);
+
+        WebMarkupContainer ajaxBusyIndicator = new WebMarkupContainer("ajaxBusyIndicator");
+        ajaxBusyIndicator.add(this.indicatorAppender);
+        add(ajaxBusyIndicator);
+
         add(new BootstrapFeedbackPanel("errorFeedbackWrapper", BootstrapFeedbackKind.ERROR));
         add(new BootstrapFeedbackPanel("warningFeedbackWrapper", BootstrapFeedbackKind.WARNING));
         add(new BootstrapFeedbackPanel("successFeedbackWrapper", BootstrapFeedbackKind.SUCCESS));
+
         add(new AccountEditorPanel("accountEditorPanel"));
         add(new AccountListPanel("accountListPanel"));
         add(new AccountControlPanel("accountControlPanel"));
@@ -28,5 +39,10 @@ public class AccountLayout extends WebPage {
         super.onConfigure();
         success("Тест");
     }
-    
+
+    @Override
+    public String getAjaxIndicatorMarkupId() {
+        return this.indicatorAppender.getMarkupId();
+    }
+
 }
--- src/main/java/promolo/wicket/account/instractructure/presentation/AccountListPanel.java
+++ src/main/java/promolo/wicket/account/instractructure/presentation/AccountListPanel.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.wicket.ajax.AjaxChannel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
+import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Radio;
+import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 
 import promolo.wicket.account.ui.AccountRecord;
 import promolo.wicket.account.ui.AccountRecordModel;
@@ -21,6 +28,8 @@ import promolo.wicket.account.ui.AccountRecordModel;
  */
 public class AccountListPanel extends Panel {
 
+    private static final AjaxChannel ACCOUNT_SELECTION_AJAX_CHANNEL = new AjaxChannel("AccountSelectionGroup", AjaxChannel.Type.DROP);
+
     @Inject
     private AccountRecordModel accountRecordModel;
 
@@ -29,22 +38,40 @@ public class AccountListPanel extends Panel {
 
         setVersioned(false);
 
-        WebMarkupContainer table = new WebMarkupContainer("table");
-        table.setOutputMarkupId(true);
+        WebMarkupContainer tableWrapper = new WebMarkupContainer("tableWrapper");
+        tableWrapper.setOutputMarkupId(true);
+
+        RadioGroup<AccountRecord> radioGroup = new RadioGroup<>("selectionGroup", new Model<AccountRecord>());
+        radioGroup.add(new AjaxFormChoiceComponentUpdatingBehavior() {
+
+            @Override
+            protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+                super.updateAjaxAttributes(attributes);
+                attributes.setChannel(ACCOUNT_SELECTION_AJAX_CHANNEL);
+            }
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                // TODO
+            }
+
+        });
+        tableWrapper.add(radioGroup);
 
         ListView<AccountRecord> listView = new ListView<AccountRecord>("row", new AccountRecordListModel()) {
 
             @Override
             protected void populateItem(ListItem<AccountRecord> item) {
+                item.add(new Radio<>("selection", item.getModel()));
                 item.add(new Label("id", item.getModelObject().getId()));
                 item.add(new Label("title", item.getModelObject().getTitle()));
             }
 
         };
         listView.setReuseItems(true);
-        table.add(listView);
+        radioGroup.add(listView);
 
-        add(table);
+        add(tableWrapper);
     }
 
     private final class AccountRecordListModel extends LoadableDetachableModel<List<AccountRecord>> {
