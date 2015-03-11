272fdd8072872ff117164ae2fbe6ada1b0ac5ea5
Alexander Morozov <alexander.v.morozov@gmail.com>
2015-03-11 23:10:17 +0600

    Кэширование Event Handler-ов объектов Presenter.

--- src/main/java/promolo/wicket/core/ui/notification/ViewUserEventListener.java
+++ src/main/java/promolo/wicket/core/ui/notification/ViewUserEventListener.java
@@ -3,6 +3,9 @@ package promolo.wicket.core.ui.notification;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
@@ -10,6 +13,7 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestHandler;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.event.IEvent;
+import org.apache.wicket.util.collections.ClassMetaCache;
 
 /**
  * TODO javadoc
@@ -18,6 +22,10 @@ import org.apache.wicket.event.IEvent;
  * @see promolo.wicket.core.stereotype.PresenterInstance
  */
 public class ViewUserEventListener extends Behavior {
+
+    private static final ClassMetaCache<Map<String, Method>> EVENT_HANDLER_MAP_CACHE = new ClassMetaCache<>();
+
+    private static final String EVENT_HANDLER_PREFIX = "on";
 
     private final Serializable presenter;
 
@@ -39,23 +47,57 @@ public class ViewUserEventListener extends Behavior {
                 .getPayload() instanceof AjaxRequestHandler)));
     }
 
-    // TODO реализация - не оптимальная, нужно использовать кэш методов-обработчиков
-    private static void forwardTo(@Nonnull Object eventSink, @Nonnull Object payload) {
-        Class<?> presenterClass = eventSink.getClass();
-        Class<?> eventClass = payload.getClass();
-        String methodName = "on" + eventClass.getSimpleName();
+    private static void forwardTo(@Nonnull Object eventSink, @Nonnull Object event) {
+        Class<?> eventSinkType = eventSink.getClass();
+        Map<String, Method> eventHandlerMap = EVENT_HANDLER_MAP_CACHE.get(eventSinkType);
+        if (eventHandlerMap == null) {
+            eventHandlerMap = mapEventHandlers(eventSinkType);
+            storeEventHandlers(eventSinkType, eventHandlerMap);
+        }
+        String methodName = eventHandlerNameByEvent(event);
+        Method eventHandler = eventHandlerMap.get(methodName);
+        if (eventHandler != null) {
+            invokeEventHandler(eventHandler, eventSink, event);
+        }
+    }
+
+    private static void storeEventHandlers(@Nonnull Class<?> eventSinkType, @Nonnull Map<String, Method> eventHandlerMap) {
+        if (eventHandlerMap.isEmpty()) {
+            EVENT_HANDLER_MAP_CACHE.put(eventSinkType, Collections.<String, Method>emptyMap());
+        } else {
+            EVENT_HANDLER_MAP_CACHE.put(eventSinkType, eventHandlerMap);
+        }
+    }
+
+    @Nonnull
+    private static Map<String, Method> mapEventHandlers(@Nonnull Class<?> eventSinkType) {
+        Map<String, Method> eventHandlerMap = new HashMap<>();
+        for (Method method : eventSinkType.getMethods()) {
+            if (method.getName().startsWith(EVENT_HANDLER_PREFIX) && method.getReturnType().equals(Void.TYPE)
+                    && method.getParameterCount() == 1) {
+                eventHandlerMap.put(method.getName(), method);
+            }
+        }
+        return eventHandlerMap;
+    }
+
+    @Nonnull
+    private static String eventHandlerNameByEvent(@Nonnull Object event) {
+        return EVENT_HANDLER_PREFIX + event.getClass().getSimpleName();
+    }
+
+    private static void invokeEventHandler(@Nonnull Method eventHandler, @Nonnull Object sink, @Nonnull Object event) {
         try {
-            Method eventHandlerMethod = presenterClass.getMethod(methodName, eventClass);
-            eventHandlerMethod.invoke(eventSink, payload);
-        } catch (NoSuchMethodException ex) {
-            // nop
+            eventHandler.invoke(sink, event);
         } catch (InvocationTargetException ex) {
             if (ex.getCause() instanceof RuntimeException) {
                 throw (RuntimeException) ex.getCause();
             }
-            throw new IllegalStateException("ошибка вызова метода " + methodName + " в классе " + presenterClass.getName(), ex.getCause());
+            throw new IllegalStateException("ошибка вызова метода " + eventHandler.getName() + " в классе " + sink.getClass().getName(),
+                    ex.getCause());
         } catch (IllegalAccessException ex) {
-            throw new IllegalStateException("метод " + methodName + " в классе " + presenterClass.getName() + " должен быть public", ex);
+            throw new IllegalStateException(
+                    "метод " + eventHandler.getName() + " в классе " + sink.getClass().getName() + " должен быть public", ex);
         }
     }
 
